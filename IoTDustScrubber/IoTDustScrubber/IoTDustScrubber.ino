1fc41b4dfe39c80222abe0c8a9073c1b072b095e
Cameron Stewart <cstewart000@gmail.com>
2018-12-02 10:48:18 +1300

    Update pin definitions refactored - added generic API calls

--- IoTDustScrubber/IoTDustScrubber/IoTDustScrubber.ino
+++ IoTDustScrubber/IoTDustScrubber/IoTDustScrubber.ino
@@ -6,13 +6,14 @@
 #include <ESP8266WiFi.h>
 
 // IoT settings
-const char* ssid = "HUAWEI-2XCNAA";
-const char* password = "95749389";
+const char* ssid = "Hackland";
+const char* password = "hackland1";
 
 const char* host = "api.pushingbox.com";
 
 // SLACK
 const String slack_hook_url = "https://hooks.slack.com/services/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";
+const String api_hook_url = "http://rocket.project-entropy.com:5000/users/1/web_requests/8/airqualsecret";
 const String slack_icon_url = "https://visualpharm.com/assets/218/Small%20Robot-595b40b85ba036ed117dc489.svg";
 const String slack_message = "test_message";
 const String slack_username = "Air quality IoT device - Scrubber";
@@ -60,21 +61,6 @@ void loop() {
 
 }
 
-void IntCallback() {
-
-  //Serial.println("Interrupt!" );
-
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    // whatever the reading is at, it's been there for longer than the debounce
-    scrubberState = !scrubberState;
-    lastDebounceTime = millis();
-
-    Serial.print("Scrubber is: " );
-    Serial.println(scrubberState, DEC);
-    postToInternet();
-  }
-
-}
 
 void psuedoInterrupt() {
 
@@ -122,6 +108,8 @@ void connectToWiFi() {
 void postToInternet() {
   String message;
 
+  
+
   message += F("The scrubber is ");
   if(scrubberState){
      message += "on";
@@ -129,8 +117,8 @@ void postToInternet() {
      message += "off";
   }
 
-  postMessageToSlack(message);
-
+  postMessageToAPI(scrubberState);
+  //postMessageToSlack(message, scrubberState);
 
 }
 
@@ -139,7 +127,48 @@ void getFromInternet() {
 }
 
 
-bool postMessageToSlack(String msg) {
+
+bool postMessageToAPI( bool scrubberState) {
+  const char* host = "rocket.project-entropy.com";
+  Serial.print("Connecting to ");
+  Serial.println(host);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClientSecure client;
+  const int httpsPort = 5000;
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("Connection failed :-(");
+    return false;
+  }
+
+  // We now create a URI for the request
+
+  Serial.print("Posting to URL: ");
+  Serial.println(api_hook_url);
+
+  String postData = "data={\"scubber_state\": " + String(scrubberState) + "}";
+
+  String url = "/users/1/web_requests/8/airqualsecret";
+ 
+  // This will send the request to the server
+  client.print(String("POST ") + api_hook_url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Content-Type: application/x-www-form-urlencoded\r\n" +
+               "Connection: close" + "\r\n" +
+               "Content-Length:" + postData.length() + "\r\n" +
+               "\r\n" + postData);
+  Serial.println("Request sent");
+  String line = client.readStringUntil('\n');
+  Serial.printf("Response code was: ");
+  Serial.println(line);
+  if (line.startsWith("HTTP/1.1 200 OK")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool postMessageToSlack(String msg, bool scrubberState) {
 
   const char* host = "hooks.slack.com";
   Serial.print("Connecting to ");
--- IoTDustSlack/IoTDustSlack.ino
+++ IoTDustSlack/IoTDustSlack.ino
@@ -1,26 +1,4 @@
 
-
-/*********************************************************************************************************
-
-  File                : DustSensor
-  Hardware Environment:
-  Build Environment   : Arduino
-  Version             : V1.0.5-r2
-  By                  : WaveShare
-
-                                   (c) Copyright 2005-2011, WaveShare
-                                        http://www.waveshare.net
-                                        http://www.waveshare.com
-                                           All Rights Reserved
-
-  Google sheet element:
-  <input type="text" class="quantumWizTextinputPaperinputInput exportInput" jsname="YPqjbf" autocomplete="off" tabindex="0" aria-label="Whats the dust level?" aria-describedby="i.desc.421979024 i.err.421979024" name="entry.1639810656" value="" dir="auto" data-initial-dir="auto" data-initial-value="" aria-invalid="false">
-entry.1639810656
-data-initial-value
-
-DHT11 tutorial
-http://www.circuitbasics.com/how-to-set-up-the-dht11-humidity-sensor-on-an-arduino/
-*********************************************************************************************************/
 #include <ESP8266WiFi.h>
 #include <DHTesp.h>
 
@@ -47,148 +25,96 @@ const String slack_username = "Air quality IoT device";
 
 
 // I/O define
-const int dht11 = D1; //temp and humidity
-const int mq2 = D2; //Gas sensor
-const int hcsr501 = D3; //PIR sensor
-const int iled = D7; //drive the led of sensor
-const int vout = A0; //analog input
+//Power pins
+const int dustSensorPowerPin = D1; //drive the led of dust sensor
+const int pirSensorPowerPin = D2; //PIR sensor Power
+const int gasSensorPowerPin = D4; //Gas sensor Power
+const int microphnePowerPin = D4; //Microphone sensor Power
+
+//read pins
+const int PIRSensorDDataPin = D3; //PIR sensor
+const int tempHumiditySensorDataPin = D5; //temp and humidity
+
+// Common Analog in NB: 3 of the sensors are analog and need to be read one at a time with diodes to isolate the circuits from each other
+const int commonAnalogInputPin = A0; //analog input
 
 
-// variable
+// status variable
 float density, voltage, temperature, humidity;
-int   adcvalue;
+int adcvalue, noiselevel, gaslevel;
+bool movementPIRSensor = false;
+
 static char outstr[15];
-bool movement = false;
+
 
 String message;
 
+// DHT object variable
 DHTesp dht;
 
 int filter(int m);
+void readAnalogSensor(int digitalPinToPower);
+void readMicrophone(int analogValue);
+void readDustSensor(int analogValue);
+void readMQ2GasSensor(int analogValue);
+void readPIRSensor();
+void readTempAndHumidySensor();
+
+//Posting functions
+void printSensorValuesToConsole();
+String compose
 
 void setup(void)
 {
 
   // initialise the DHT
-  dht.setup(dht11, DHTesp::DHT11);
+  dht.setup(tempHumiditySensorDataPin, DHTesp::tempHumiditySensorDataPin);
 
-  
-  pinMode(iled, OUTPUT);
-  digitalWrite(iled, LOW); //iled default closed
+  // Initialise outputs and modes
+  pinMode(dustSensorPowerPin, OUTPUT);
+  digitalWrite(dustSensorPowerPin, LOW);
+
+  pinMode(pirSensorPowerPin, OUTPUT);
+  digitalWrite(pirSensorPowerPin, LOW);
+
+  pinMode(gasSensorPowerPin, OUTPUT);
+  digitalWrite(gasSensorPowerPin, LOW);
+
+  // Initialise Serial
   Serial.begin(9600);  //send and receive at 9600 baud
-  Serial.print("Dust Sensor");
 
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
-     would try to act as both a client and an access-point and could cause
-     network-issues with your other WiFi-devices on your WiFi-network. */
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 }
 
 void loop(void)
 {
   // DHT 
   delay(dht.getMinimumSamplingPeriod());
+  Serial.printf("dht sampling period: %d\n" ,dht.getMinimumSamplingPeriod());
 
-  // Read temp and humidity
-  //int chk = DHT.read11(dht11);
-  temperature = dht.getTemperature();
-  humidity = dht.getHumidity();
+  readTempAndHumidySensor();
+  
+  // check movementPIRSensor
+  readPIRSensor();
 
-  // check movement
-  movement = digitalRead(hcsr501);
-
+  //Michrophone
+  readMichrophone(readAnalogSensor(microphnePowerPin));
   
   // read particle sensor
-  digitalWrite(iled, HIGH);
-  delayMicroseconds(280);
-  adcvalue = analogRead(vout);
-  digitalWrite(iled, LOW);
+  readDustSensor(readAnalogSensor(dustSensorPowerPin));
 
-  adcvalue = filter(adcvalue);
-  voltage = (SYS_VOLTAGE / 1024.0) * adcvalue * 11;// convert voltage (mv)
+   // read particle sensor
+  readMQ2GasSensor(readAnalogSensor(gasSensorPowerPin)); 
 
-  // voltage to density
-  if (voltage >= NO_DUST_VOLTAGE)
-  {
-    voltage -= NO_DUST_VOLTAGE;
-    density = voltage * COV_RATIO;
-  }
-  else
-    density = 0;
+  // Debug sensor values to console
+  printSensorValuesToConsole();
+  String message = composePostMessage();
 
-  dtostrf(density , 7, 3, outstr);
-
-  // Print temp and humidity
-  Serial.print("The current temperature is: ");
-  Serial.print(temperature, 1);
-  Serial.print(" 'C, humidity: ");
-  Serial.println(humidity, 1);
-
-  // Print Gas
-  Serial.print("The current temperature is: ");
-  Serial.print(temperature);
-  Serial.print(" 'C, humidity: ");
-  Serial.println(humidity);
-  
-  // Print movement
-  Serial.print("Movement detected: ");
-  Serial.println(movement);
-
-
-  // print result to console - post to internet
-  Serial.print("The current dust concentration is: ");
-  Serial.print(density);
-  Serial.print(" ug/m3\n");
-
- /*
-  char outstrMovement[10];
-  dtostrf(movement , 7, 3, outstrMovement);
-
-  char outstrDensity[10];
-  dtostrf(density , 7, 3, outstrDensity);
-  
-  //message += "Movement detected: " + outstrMovement + "\n";
-  message += "The current dust concentration is: "+  density + " ug/m3\n";
-  */
-
-  message += F("Movement detected: ");
-  message += String(movement, 2);
-  message += F("\n");
-
-  message += F("The current dust concentration is: ");
-  message += String(density, 6);
-  message += F(" ug/m3\n");
-
-  message += F("The temperature is: ");
-  message += String(temperature, 1);
-  message += F(" 'C\n");
-  
-  message += F("The humidity is: ");
-  message += String(humidity, 1);
-  message += F(" %\n");
-    
   postMessageToSlack(message);
   
-  delay(3600000);
+  delay(60000);
 }
 
-/*
-  private function
-*/
+//filter for dust sensor values.
 int filter(int m) {
   static int flag_first = 0, _buff[10], sum;
   const int _buff_max = 10;
@@ -221,6 +147,32 @@ int filter(int m) {
 }
 
 
+void connectToInternet(){
+
+  Serial.print("Dust Sensor");
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+}
+
+
 bool postMessageToSlack(String msg)
 {
   const char* host = "hooks.slack.com";
@@ -231,7 +183,7 @@ bool postMessageToSlack(String msg)
   WiFiClientSecure client;
   const int httpsPort = 443;
   if (!client.connect(host, httpsPort)) {
-    Serial.println("Connection failed :-(");
+    Serial.println("Connection fadustSensorPowerPin :-(");
     return false;
   }
 
@@ -260,17 +212,107 @@ bool postMessageToSlack(String msg)
   }
 }
 
+int readAnalogSensor(int digitalPinToPower){
+
+  digitalWrite(digitalPinToPower,HIGH);
+  delay(300); //TODO check delay on function
+
+  int anologValue = analogRead(commonAnalogInputPin);
+
+  digitalWrite(digitalPinToPower,LOW);
+
+  //Degu
+  Serial.printf("analog vlaue of: %d read on pin %d", anologValue, digitalPinToPower);
+
+  return anologValue;
+  
+}
+
+void readMicrophone(int analogValue){
+
+  noiselevel = analogValue;
+  
+}
+
+void readMQ2GasSensor(int analogValue){
+
+  gaslevel = analogValue;
+}
+
+void readDustSensor(int analogValue){
 
 
+  adcvalue = filter(analogValue);
+  voltage = (SYS_VOLTAGE / 1024.0) * adcvalue * 11;// convert voltage (mv)
+
+  // voltage to density
+  if (voltage >= NO_DUST_VOLTAGE)
+  {
+    voltage -= NO_DUST_VOLTAGE;
+    density = voltage * COV_RATIO;
+  }
+  else
+    density = 0;
 
 
+}
+
+void readPIRSensor(){
+
+  movementPIRSensor = digitalRead(PIRSensorDDataPin);
+
+}
+void readTempAndHumidySensor(){
+    // Read temp and humidity
+  //int chk = DHT.read11(tempHumiditySensorDataPin);
+  temperature = dht.getTemperature();
+  humidity = dht.getHumidity();
+}
 
 
+// Print temp and humidity
+void printSensorValuesToConsole(){
+  Serial.print("The current temperature is: ");
+  Serial.print(temperature, 1);
+  Serial.print(" 'C, humidity: ");
+  Serial.println(humidity, 1);
+
+  // Print Gas
+  Serial.print("The current temperature is: ");
+  Serial.print(temperature);
+  Serial.print(" 'C, humidity: ");
+  Serial.println(humidity);
+  
+  // Print movementPIRSensor
+  Serial.print("movementPIRSensor detected: ");
+  Serial.println(movementPIRSensor);
 
 
+  // print result to console - post to internet
+  Serial.print("The current dust concentration is: ");
+  Serial.print(density);
+  Serial.print(" ug/m3\n");
+}
 
+String composePostMessage(){
 
+ message += F("movementPIRSensor detected: ");
+  message += String(movementPIRSensor, 2);
+  message += F("\n");
 
+  message += F("The current dust concentration is: ");
+  message += String(density, 6);
+  message += F(" ug/m3\n");
+
+  message += F("The temperature is: ");
+  message += String(temperature, 1);
+  message += F(" 'C\n");
+  
+  message += F("The humidity is: ");
+  message += String(humidity, 1);
+  message += F(" %\n");
+    
+}
 
 
 
