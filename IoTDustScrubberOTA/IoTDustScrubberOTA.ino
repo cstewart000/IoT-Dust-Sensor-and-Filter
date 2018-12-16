6d62e186ed42e200cd733e4e89c055c0636ec790
Cameron Stewart <cstewart000@gmail.com>
2018-12-16 19:31:16 +1300

    Updated Header

--- IoTDustScrubberOTA/IoTDustScrubberOTA.ino
+++ IoTDustScrubberOTA/IoTDustScrubberOTA.ino
@@ -1,29 +1,43 @@
 
 /*
-  A Program to Switch on an appliance manually and from the internet. 
+  A Program to Switch on an dust scrubber manually and from the internet. ]
+  Can be adapted for any AC appliance with a push button start stop.
 
-  Circuit
-  Push button 
-  SS Relay
-  5v Power supply unit - to provide power to NodeMCU
+  Webserver instance run to enable http get requests to change state. Also suppurts over the air (OTA) firware updates.
+  Webserver allows get request to switch on/off the relay. State of the circuit (SSR) is posted to a web agent hosted on 
+  http://rocket.project-entropy.com
 
+  Code written by: Cameron Stewart, cstewart000@gmail.com
+  code hosted at: https://github.com/cstewart000/IoT-Dust-Sensor-and-Filter/blob/master/IoTDustScrubberOTA/IoTDustScrubberOTA.ino
+  Blcg post of build at: https://hackingismakingisengineering.wordpress.com/2018/12/09/upgrading-an-air-filter-to-iot/
 
-http://rocket.project-entropy.com:5000/agents
-http://rocket.project-entropy.com:5000/users/1/web_requests/8/airqualsecret
+  Source material
+  Example code "BasicOTA" and the youtube video from ACROBOTIC provided the basis for the OTA cfunction.
+  https://www.youtube.com/watch?v=3aB85PuOQhY
 
-
-     //Youtube video ACROBOTIC
-     https://www.youtube.com/watch?v=3aB85PuOQhY
-
-     Python 2.7 (python 3.5 not supported) – https://www.python.org/
+  Python 2.7 (python 3.5 not supported) – https://www.python.org/
   Note: Windows users MUST select “Add python.exe to Path” while installing python 2.7 .
 
+  OTA Upload instructions
+  ESP8266 must be connected to THE SAME network to upload codes.
+  Make a request to the board to restart - this will make it receptive to new code OTA http://192.168.1.4/restart
+  Select the network path as the COM port
+  Upload the code from the IDE (you have a short time window after the restart has been called).
+  
+  Circuit Description
+  + Push button with  pull down resister
+  + Solid State Relay
+  + 5v Power supply unit - to provide power to NodeMCU
+  + Fuse
 
-   Upload instructions
-   ESP8266 must be connected to THE SAME network to upload codes.
-   Make a request to the board to restart - this will make it receptive to new code OTA http://192.168.1.4/restart
-   Select the network path as the COM port
-   Upload the code from the IDE
+  The 5v power supply is wired to the mains through the fuse.
+  The live and neutral cores of the live Core is wired in parallel to the relay and spade terminals connect the live termional of 
+  the motor to the realay. Spade terminals also connect the earth and neutral cores with allows the motor wiring loom to be 
+  separated from the control circuitry.
+  The case, motor, PSU and Mains are all earthed together on a binding post.  
+
+  Wbhook for THIS PROJECT:
+  http://rocket.project-entropy.com:5000/users/1/web_requests/8/airqualsecret
 
 */
 
@@ -38,10 +52,11 @@ http://rocket.project-entropy.com:5000/users/1/web_requests/8/airqualsecret
 const char* ssid = "HUAWEI-2XCNAA";
 const char* password = "95749389";
 
-const char* POST_REQUEST = "http://rocket.project-entropy.com:5000/users/1/web_requests/8/airqualsecret";
-
 //const char* ssid = "Hackland";
 //const char* password = "hackland1";
+
+// TODO: Update to your h\web hook 
+const char* WEB_HOOK = "http://rocket.project-entropy.com:5000/users/1/web_requests/8/airqualsecret";
 
 //To enable OTA uploading, need web server
 ESP8266WebServer server;
@@ -59,7 +74,7 @@ int buttonState;             // the current reading from the input pin
 int lastButtonState = LOW;   // the previous reading from the input pin
 
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 2000;    // the debounce time; increase if the output flickers
+unsigned long DEBOUNCE_DELAY = 2000;    // the debounce time; increase if the output flickers
 
 void setup() {
 
@@ -167,7 +182,7 @@ void psuedoInterrupt() {
 
     //Serial.println("Interrupt!" );
 
-    if ((millis() - lastDebounceTime) > debounceDelay) {
+    if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
       // whatever the reading is at, it's been there for longer than the debounce
       scrubberState = !scrubberState;
       lastDebounceTime = millis();
@@ -182,12 +197,12 @@ void psuedoInterrupt() {
 bool postMessageToAPI( bool scrubberState) {
   const char* host = "rocket.project-entropy.com";
   Serial.print("Connecting to ");
-  Serial.println(POST_REQUEST);
+  Serial.println(WEB_HOOK);
 
   // Use WiFiClient class to create TCP connections
   WiFiClientSecure client;
   const int httpsPort = 5000;
-  if (!client.connect(POST_REQUEST, httpsPort)) {
+  if (!client.connect(WEB_HOOK, httpsPort)) {
     Serial.println("Connection failed :-(");
     return false;
   }
@@ -197,7 +212,7 @@ bool postMessageToAPI( bool scrubberState) {
   //String url = "/users/1/web_requests/8/airqualsecret";
  
   // This will send the request to the server
- client.print(String("POST ") + POST_REQUEST + " HTTP/1.1\r\n" +
+ client.print(String("POST ") + WEB_HOOK + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
                "Content-Type: application/x-www-form-urlencoded\r\n" +
                "Connection: close" + "\r\n" +
